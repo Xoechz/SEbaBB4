30b1e4909eb3202790aecd47f5e734a231cccfb9
Elias Leonhardsberger <elias.leonhardsberger@gmail.com>
2025-03-15 11:04:28 +0100

    imported end of lesson state

--- SCR/site/index.php
+++ SCR/site/index.php
@@ -1,14 +1,17 @@
 <?php
-require_once('inc/bootstrap.php');
 
-$view = $_default_view;
+use Bookshop\Book;
+use Bookshop\Category;
+
+require_once ('inc/bootstrap.php');
+
+$view = $default_view;
 
 if (isset($_REQUEST['view'])
     && !empty($_REQUEST['view'])
-    && file_exists('views/' . $_REQUEST['view'] . '.php'))
-{
+    && file_exists(__DIR__ . '/views/' . $_REQUEST['view'] . '.php')) {
+
     $view = $_REQUEST['view'];
 }
 
-require_once('views/' . $view . '.php');
-?>
+require_once ('views/' . $view . '.php');
